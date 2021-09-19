f56e7ed8a359eb23bcb3874f0e021a469c00f874
Agung Firmansyah <agungfirid@gmail.com>
2021-09-19 20:26:03 +0700

    fix:disable cors

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+var cors = require('cors');
+
 const PORT = process.env.PORT || 3000;
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(function (req, res, next) {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'https://payment-monitoring.herokuapp.com'
-  ); // update to match the domain you will make the request from
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+app.use(cors());
 // parse application/json
 app.use(bodyParser.json());
 app.get('/', (req, res) => {

16ce6623a3bfedf497d9587e999904db99e3042a
Agung Firmansyah <agungfirid@gmail.com>
2021-09-19 20:17:23 +0700

    fix: cors

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(function (req, res, next) {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://payment-monitoring.herokuapp.com'
+  ); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 // parse application/json
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
